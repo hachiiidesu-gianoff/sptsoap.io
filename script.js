eda8308c1dc7d86c6d0fc566c4de2606ca2e4923
hachii <143150588+hachiiidesu-gianoff@users.noreply.github.com>
2023-09-17 11:32:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav');
-
-menu_btn.addEventListener('click', function () {
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-});
-
-const form = document.getElementById('contact-form');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const name = form.querySelector('#name').value;
-  const email = form.querySelector('#email').value;
-  const message = form.querySelector('#message').value;
-  const url = `mailto:tolentinogian150@gmail.com?subject=Message from ${name} (${email})&body=${message}`;
-  window.location.href = url;
-});
-
 function myFunction() {
   window.alert("Are you ok?");
-  window.alert("Hello World");
-  window.alert("Love Yourself");
-  window.alert("I love You")
+  window.alert("If you're not then, lean on me I will listen to your problem");
+  window.alert("Don't think of suicide, Love Yourself");
+  window.alert("Cheer up ok?")
 }
